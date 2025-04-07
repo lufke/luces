d5d4d791ecbf66748180f83cc0983ecc6d8a597c
Felipe Valenzuela <nosmofui@gmail.com>
2025-04-07 00:13:02 -0400

    se agrega componente Lora

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { ListaLuces } from '../components/ListaLuces'
 import { DHT11 } from '../components/DHT11'
 import { tasmotaContext } from '../contexts/tasmotaContext'
 import { useContext } from 'react'
+import Lora from '../components/Lora' 
 
 export const HomePage = () => {
 
@@ -13,6 +14,7 @@ export const HomePage = () => {
                 : <>
                     {knilStats.temp && <DHT11 />}
                     <ListaLuces />
+                    <Lora />
                 </>
             }
         </div>
